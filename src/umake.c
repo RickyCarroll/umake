8c9b10cd1a0b891ccf748b58f6b1c73d51b41114
Richard Carroll <carrolr7@wwu.edu>
2018-10-10 15:19:30 -0700

    parses arguments

--- src/umake.c
+++ src/umake.c
@@ -8,22 +8,39 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <ctype.h>
+#include <stdbool.h>
 
 /* CONSTANTS */
 
-
 /* PROTOTYPES */
+
+/* GLOBALS */
+ssize_t linelen;
 
 /* Process Line
  * line   The command line to execute.
  * This function interprets line as a command line.  It creates a new child
  * process to execute the line and waits for that process to complete. 
  */
-void processline(char* line);
+void   processline(char* line);
+
+/* Arg parse
+ * line   The command line to process
+ */
+
+char** arg_parse(char* line);
+
+
+/* Count args
+ * line   The command line count number of args
+ * This function counts the number of arguments in the line.
+ */
+int    countargs(char* line);
 
 /* Main entry point.
  * argc    A count of command-line arguments 
- * argv    The command-line argument valus
+ * argv    The com mand-line argument valus
  *
  * Micro-make (umake) reads from the uMakefile in the current working
  * directory.  The file is read one line at a time.  Lines with a leading tab
@@ -31,12 +48,11 @@ void processline(char* line);
  * the leading tab.
  */
 int main(int argc, const char* argv[]) {
-
+  
   FILE* makefile = fopen("./uMakefile", "r");
-
   size_t  bufsize = 0;
   char*   line    = NULL;
-  ssize_t linelen = getline(&line, &bufsize, makefile);
+  linelen = getline(&line, &bufsize, makefile);
   
   while(-1 != linelen) {
 
@@ -47,7 +63,6 @@ int main(int argc, const char* argv[]) {
 
     if(line[0] == '\t') 
       processline(&line[1]);
-
 
     linelen = getline(&line, &bufsize, makefile);
   }
@@ -61,22 +76,27 @@ int main(int argc, const char* argv[]) {
  * 
  */
 void processline (char* line) {
-  
+
+  char** args = arg_parse(line);
   const pid_t cpid = fork();
   switch(cpid) {
-
+    
   case -1: {
     perror("fork");
     break;
   }
-
+    
   case 0: {
-    execlp(line, line, (char*)(0));
-    perror("execlp");
+    
+    execvp(args[0], args);
+    /*
+      execlp(line, line, (char*)(0));
+    */
+    perror("execvp");
     exit(EXIT_FAILURE);
     break;
   }
-
+    
   default: {
     int   status;
     const pid_t pid = wait(&status);
@@ -91,3 +111,50 @@ void processline (char* line) {
   }
   }
 }
+
+char** arg_parse(char* line){
+  int numargs = countargs(line) + 1;
+  char** args = malloc(numargs*sizeof(char*));
+  
+  bool first = true;
+  int lcount = 0;
+  int acount = numargs;
+  while (linelen > lcount){
+    //only interested in args
+    while (isalnum(line[lcount])){
+      if (first){
+	args[numargs - acount] = &line[lcount];
+	first = false;
+	acount--;
+      }
+      lcount++;
+    }
+    //replace whitespace with null char
+    line[lcount] = '\0';
+    lcount++;
+    first = true;
+  }
+  return args;
+}
+
+int countargs(char* line){  
+  char* count = line;
+  int ix = 0;
+  int numargs = 0;
+  while(count[ix] != '\0'){
+    //loop through whitespace
+    while(!isalnum(count[ix])){
+      ix++;
+    }
+    //first letter if not NULL
+    if (count[ix] != '\0'){
+      numargs++;
+    }
+    //loop through arg
+    while(isalnum(count[ix])){
+      ix++;
+    }
+  }
+  return numargs;
+}
+
